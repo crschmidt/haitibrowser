559a8d9e2bc2ea68c36eb4f3d87bcec65477c82b
Christopher Schmidt <crschmidt@crschmidt.net>
2010-01-25 23:50:16 -0500

    adding start of edit UI work.

---
+++ js/Edit.js
@@ -0,0 +1,24 @@
+if (!window.H) { H = {}; }
+H.EditToolbar  = OpenLayers.Class(OpenLayers.Control.EditingToolbar, {
+    initialize: function() {
+        OpenLayers.Control.EditingToolbar.prototype.initialize.apply(this, arguments);
+        var save = new OpenLayers.Control.Button({
+            trigger: function() { alert('abc')},
+            displayClass: 'olControlSaveFeatures'
+        });    
+        this.addControls([save]);
+    },
+    CLASS_NAME: 'OpenLayers.Control.EditingToolbar'
+});
+H.Edit = OpenLayers.Class(OpenLayers.Control, {
+    activate: function() {
+        this.layer = new OpenLayers.Layer.Vector("Edit Layer");
+        this.map.addLayer(this.layer);
+        this.toolbar = new H.EditToolbar(this.layer);
+        this.map.addControl(this.toolbar);
+    },
+    deactivate: function() {
+        this.map.removeControl(this.toolbar);
+    },
+    CLASS_NAME: 'H.Edit'
+});    
